382ae10cefeb74904b2444bfb8aaf29ef5d19938
rigwild <contact@asauvage.fr>
2018-06-26 23:25:32 +0200

    Scrapper and parser working, need the crawl bot

--- scrapper.js
+++ scrapper.js
@@ -1,79 +1,79 @@
 const https = require('https');
 
 const regexList = {
-	getDayGroups: /message_group\" id=\"(.*?)\"\>[\s\S]*?(?=message_group|pagination)/g,
-	getDayDate: /id=\"(.*?)\"/,
-	getEventTypes: /message (?:auto-message)*(.*?)\"/g
+  getDayGroups: /message_group\" id=\"(.*?)\"\>[\s\S]*?(?=message_group|pagination)/g,
+  getDayDate: /id=\"(.*?)\"/,
+  getEventTypes: /message (?:auto-message)*(.*?)\"/g
 };
 
 module.exports.fetchStatusPage = date =>
-	new Promise((resolve, reject) => {
-		const url = date ? `https://status.github.com/messages/${date}` : "https://status.github.com/messages";
-		https.get(url, res => {
-			const statusCode = res.statusCode;
-			const contentType = res.headers['content-type'];
+  new Promise((resolve, reject) => {
+    const url = date ? `https://status.github.com/messages/${date}` : "https://status.github.com/messages";
+    https.get(url, res => {
+      const statusCode = res.statusCode;
+      const contentType = res.headers['content-type'];
 
-			if (statusCode !== 200) {
-				console.error(new Error(`Request Failed.\n Status Code: ${statusCode}`).message);
-				res.resume();
-				return;
-			}
-			res.setEncoding('utf8');
-			let rawData = '';
-			res.on('data', chunk => rawData += chunk);
-			res.on('end', () => resolve(rawData));
-		}).on('error', (e) => reject(e.message));
-	});
+      if (statusCode !== 200) {
+        console.error(new Error(`Request Failed.\n Status Code: ${statusCode}`).message);
+        res.resume();
+        return;
+      }
+      res.setEncoding('utf8');
+      let rawData = '';
+      res.on('data', chunk => rawData += chunk);
+      res.on('end', () => resolve(rawData));
+    }).on('error', (e) => reject(e.message));
+  });
 
 module.exports.parseStatusPage = html => {
-	let result = [];
-	let temp, dayList = [];
-	//Separate each days of the page
-	while ((temp = regexList.getDayGroups.exec(html)))
-		dayList.push(temp[0])
+  let result = [];
+  let temp, dayList = [];
+  //Separate each days of the page
+  while ((temp = regexList.getDayGroups.exec(html)))
+    dayList.push(temp[0])
 
-	if (dayList.length === 0)
-		return;
+  if (dayList.length === 0)
+    return;
 
-	dayList.forEach(day => {
-		let dayDate, eventsType = [];
-		//Get the date of the current day
-		try {
-			dayDate = day.match(regexList.getDayDate)[1];
-		} catch (e) {
-			console.error(e);
-			dayDate = null;
-		}
+  dayList.forEach(day => {
+    let dayDate, eventsType = [];
+    //Get the date of the current day
+    try {
+      dayDate = day.match(regexList.getDayDate)[1];
+    } catch (e) {
+      console.error(e);
+      dayDate = null;
+    }
 
-		let dayResult = {
-			date: dayDate,
-			events: {
-				good: 0,
-				minor: 0,
-				major: 0
-			}
-		};
-		//get the events type
-		while ((temp = regexList.getEventTypes.exec(day)))
-			eventsType.push(temp[1].trim());
+    let dayResult = {
+      date: dayDate,
+      events: {
+        good: 0,
+        minor: 0,
+        major: 0
+      }
+    };
+    //get the events type
+    while ((temp = regexList.getEventTypes.exec(day)))
+      eventsType.push(temp[1].trim());
 
-		//Increment the count of the corresponding event type
-		eventsType.forEach(aEventType => {
-			switch (aEventType) {
-				case "good":
-					dayResult.events.good++;
-					break;
-				case "minor":
-					dayResult.events.minor++;
-					break;
-				case "major":
-					dayResult.events.major++;
-					break;
-				default:
-					break;
-			}
-		});
-		result.push(dayResult);
-	});
-	return result;
+    //Increment the count of the corresponding event type
+    eventsType.forEach(aEventType => {
+      switch (aEventType) {
+        case "good":
+          dayResult.events.good++;
+          break;
+        case "minor":
+          dayResult.events.minor++;
+          break;
+        case "major":
+          dayResult.events.major++;
+          break;
+        default:
+          break;
+      }
+    });
+    result.push(dayResult);
+  });
+  return result;
 };
--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const http = require('http');
 const scrapper = require('./scrapper.js');
 
 const startServer = () => {
-	http.createServer(function(req, res) {
-		res.writeHead(200, {
-			'Content-Type': 'text/html'
-		});
-		res.write('Hello World!');
-		res.end();
-	}).listen(8080);
-	console.log("Server is listening on 127.0.0.1:8080");
+  http.createServer(function(req, res) {
+    res.writeHead(200, {
+      'Content-Type': 'text/html'
+    });
+    res.write('Hello World!');
+    res.end();
+  }).listen(8080);
+  console.log("Server is listening on 127.0.0.1:8080");
 };
 
 scrapper.fetchStatusPage("2010-06-12")
-	.then(res => console.log(scrapper.parseStatusPage(res)))
-	.catch(err => console.log(err));
+  .then(res => console.log(scrapper.parseStatusPage(res)))
+  .catch(err => console.log(err));
